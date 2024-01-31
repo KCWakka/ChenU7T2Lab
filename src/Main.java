bbc2ee40144b86a830daf88c88e8fe29de8db63d
BT_2N2_06 <sample@gmail.com>
2024-01-31 13:34:32 -0500

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,97 @@
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        ArrayList<Integer> integer = new ArrayList<Integer>();
+//        Integer int1 = 10;
+//        Integer int2 = 5;
+//        Integer int3 = 24;
+//        integer.add(int1);
+//        integer.add(int2);
+//        integer.add(int3);
+//        System.out.println(integer);
+//
+//        Integer num1 = new Integer(5);
+//        Integer num2 = 10;
+//        int num3 = 15;
+//
+////        ArrayList<Integer> numList = new ArrayList<Integer>();
+////
+////        numList.add(num1);
+////        numList.add(num2);
+////        numList.add(num3);
+////
+////        int sum = num1 + num2 + num3;
+////
+////        numList.add(sum);
+////
+////        System.out.println(numList);
+//
+//        ArrayList<String> letters = new ArrayList<String>();
+//
+//        letters.add("A");
+//        letters.add("B");
+//        letters.add("C");
+//        System.out.println(letters);
+//        System.out.println(letters.size());
+//        letters.add(0, "D");
+//        System.out.println(letters);
+//        System.out.println(letters.size());
+//        letters.add(3, "E");
+//        System.out.println(letters);
+//        System.out.println(letters.size());
+//        letters.add(1, "F");
+//        letters.add(1, "G");
+//        letters.add(1, "H");
+//        System.out.println(letters);
+//        System.out.println(letters.size());
+//        System.out.println("-------------");
+//        letters.remove(2);
+//        System.out.println(letters);
+//        System.out.println(letters.size());
+//        letters.remove(3);
+//        System.out.println(letters);
+//        System.out.println(letters.size());
+//        letters.remove(0);
+//        letters.remove(1);
+//        System.out.println(letters);
+//        System.out.println(letters.size());
+//        System.out.println("-------------");
+//
+//        ArrayList<Integer> numList = new ArrayList<Integer>();
+//
+//
+//        numList.add(3);
+//
+//
+//        numList.add(0, 6);
+//
+//
+//        numList.add(7);
+//
+//        numList.add(2, 8);
+//
+//
+//        numList.set(3, 9);
+//
+//
+//        numList.add(1, 2);
+//
+//        numList.add(1, numList.size());
+//
+//        numList.remove(4);
+//
+//        numList.set(4, 0);
+//
+//
+//        System.out.println(numList);
+//
+//
+        ArrayList<String> test = new ArrayList<String>();
+        test.add("a");
+        test.add(0, "b");
+        test.add(1, "c");
+        test.remove(2);
+        test.set(0, "d");
+        System.out.println(test);
     }
 }
--- src/main2.java
+++ src/main2.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class main2 {
+import java.util.ArrayList;
+import java.util.Arrays;
+public class main2 {
+    public static void main(String[] args) {
+        // create an array with the known values
+        Integer[] intsArray = {1, 2, 3, 4, 5};
+
+// use the Arrays.asList utility method to convert the array to a list "inline"
+        ArrayList<Integer> intList = new ArrayList<Integer>(Arrays.asList(intsArray));
+        System.out.println(intList);
+        Car car1 = new Car("a", 1);
+        Car car2 = new Car("b", 2);
+        Car car3 = new Car("c", 3);
+        Car[] cars = {car1, car2, car3};
+        ArrayList<Car> carList = new ArrayList<Car>(Arrays.asList(cars));
+        System.out.println(carList);
+    }
 }
